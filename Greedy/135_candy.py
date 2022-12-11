57d1342bf930f32ade630e514f4ab3daa6bd38b9
Md. Riyadul Hasan <riadulhasancse48@gmail.com>
2022-12-11 19:37:38 +0600

    Create 135_candy.py

---
+++ Greedy/135_candy.py
@@ -0,0 +1,10 @@
+class Solution:
+    def candy(self, ratings) -> int:
+        res = [1]*len(ratings)
+        for i in range(1,len(ratings)):
+            if ratings[i]>ratings[i-1]:
+                res[i]=res[i-1]+1
+        for i in range(len(ratings)-1,0,-1):
+            if ratings[i-1]>ratings[i]:
+                res[i-1]=max(res[i-1],res[i]+1)
+        return sum(res)
