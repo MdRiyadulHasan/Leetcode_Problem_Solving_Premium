88a822468de469a29816967720e1de4704d7d0b6
Md. Riyadul Hasan <riadulhasancse48@gmail.com>
2022-12-18 14:38:40 +0600

    Create 224_basic_calculator.py

---
+++ Math/224_basic_calculator.py
@@ -0,0 +1,35 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        
+        num=0
+        res=0
+        sign=1
+        stack=[]
+        
+        for char in s:
+            if char.isdigit():
+                num=num*10+int(char)
+            elif char in ["-","+"]:
+                res=res+num*sign
+                num=0
+                if char=="-":
+                    sign=-1
+                else:
+                    sign=1
+            elif char=="(":
+                stack.append(res)
+                stack.append(sign)
+                sign=1
+                res=0
+            elif char==")":
+                res+=sign*num
+                res*=stack.pop()## process sign
+                res+=stack.pop() ##process with old value
+                num=0
+        
+        return res+num*sign
+if __name__ =='__main__':
+    s = "(26-(4+5-12)-3)+(18-3)"
+    ob=Solution()
+    ans = ob.calculate(s)
+    print(ans)
