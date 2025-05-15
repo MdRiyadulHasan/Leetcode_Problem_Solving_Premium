8bb3334417907c84eda860c0fb5ffdb3ea1839f2
Md. Riyadul Hasan <riadulhasancse48@gmail.com>
2025-05-15 16:06:44 +0600

    Create 70_climbing_stairs.py

---
+++ Dynamic_Programming/70_climbing_stairs.py
@@ -0,0 +1,11 @@
+def climbStairs(n):
+    if n<=2:
+        return n
+    dp = [0]*(n+1)
+    dp[1]=1
+    dp[2]=2
+    for i in range(3, n+1):
+        dp[i]=dp[i-1]+dp[i-2]
+    return dp[-1]
+n=3
+print(climbStairs(n))
