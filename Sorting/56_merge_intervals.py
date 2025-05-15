455349ff08af01759c4df70793e281111527592a
Md. Riyadul Hasan <riadulhasancse48@gmail.com>
2025-05-15 13:17:32 +0600

    Update 56_merge_intervals.py

--- Sorting/56_merge_intervals.py
+++ Sorting/56_merge_intervals.py
@@ -1,13 +1,13 @@
 class Solution:
-    def merge(self, intervals):
-        intervals.sort()
-        res = []
-        for i in intervals:
-            if not res or res[-1][1] < i[0]:
-                res.append(i)
+   def merge(intervals):
+        result =[]
+        intervals.sort(key=lambda x:x[0])
+        for interval in intervals:
+            if not result or result[-1][1]<interval[0]:
+                result.append(interval)
             else:
-                res[-1][1] = max(res[-1][1], i[1])
-        return res
+                result[-1][1] = max(result[-1][1], interval[1])
+        return result
 if __name__ == '__main__':
     intervals = [[1,3],[2,6],[8,10],[15,18]]
     ob = Solution()
