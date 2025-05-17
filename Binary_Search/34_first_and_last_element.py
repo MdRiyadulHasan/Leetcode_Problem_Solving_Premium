a5d2e5adf8fbf655539559393de1b64a9ddf9be5
Md. Riyadul Hasan <riadulhasancse48@gmail.com>
2025-05-17 15:57:52 +0600

    Update 34_first_and_last_element.py

--- Binary_Search/34_first_and_last_element.py
+++ Binary_Search/34_first_and_last_element.py
@@ -1,23 +1,37 @@
-class Solution:
-    def searchRange(self, nums, target: int):
-        left = self.binary_search(nums,target,True)
-        right = self.binary_search(nums,target,False)
-        return [left,right]
-        
-    def binary_search(self, nums,target,leftBias):
-        i=-1
-        l=0
+def searchRange(nums, target):
+    def firstPosition(nums):
+        l = 0
         r = len(nums)-1
+        res = -1
         while l<=r:
             m = (l+r)//2
-            if nums[m]<target:
-                l=m+1
-            elif nums[m]>target:
-                r = m-1
+            if target==nums[m]:
+                res=m
+                r=m-1
+            elif nums[m]<target:
+                l = m+1
             else:
-                i = m
-                if leftBias:
-                    r = m-1
-                else:
-                    l=m+1
-        return i
+                r = m-1
+        return res
+    def lastPosition(nums):
+        l = 0
+        r = len(nums)-1
+        res = -1
+        while l<=r:
+            m = (l+r)//2
+            if target==nums[m]:
+                res=m
+                l = m+1
+            elif nums[m]<target:
+                l = m+1
+            else:
+                r = m-1
+        return res
+        
+    first = firstPosition(nums)
+    last = lastPosition(nums)
+    return [first, last]
+    
+nums = [5,7,8,8,8,8,8,10]
+target =8
+print(searchRange(nums, target))
