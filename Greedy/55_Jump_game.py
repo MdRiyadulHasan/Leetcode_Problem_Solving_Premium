af61ca253d015f9b3124acf72c124d72edff3980
Md. Riyadul Hasan <riadulhasancse48@gmail.com>
2025-05-18 09:05:26 +0600

    Create 55_Jump_game.py

---
+++ Greedy/55_Jump_game.py
@@ -0,0 +1,10 @@
+def jumpGame(nums):
+    maxReachable = 0
+    for i in range(len(nums)):
+        if i>maxReachable:
+            return False
+        maxReachable = max(maxReachable, i+nums[i])
+    return True
+     
+nums = [3,2,1,0,4]
+print(jumpGame(nums))
