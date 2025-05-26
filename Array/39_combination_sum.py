6b9dc43260f06204f63b4ab247e6f79db55e3a88
Md. Riyadul Hasan <46715117+MdRiyadulHasan@users.noreply.github.com>
2025-05-26 13:05:37 +0600

    Update 39_combination_sum.py

--- Array/39_combination_sum.py
+++ Array/39_combination_sum.py
@@ -1,1 +1,15 @@
-
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        result = []
+        def dfs(candidates, path, target):
+            if target==0:
+                result.append(path)
+                return 
+            for i in range(len(candidates)):
+                if target-candidates[i]<0:
+                    continue
+                dfs(candidates[i:], path+[candidates[i]], target-candidates[i])
+            
+        dfs(candidates, [], target)
+        return result
+        
